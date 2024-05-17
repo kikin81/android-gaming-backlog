c4a805497f046eb1177e7373c02e6507359853c3
Francisco <fvelazquez@turo.com>
2024-05-17 05:31:49 +0000

    style: adds splashcreen dependency and theme

--- app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
+++ app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import us.kikin.android.gamingbacklog.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme {
