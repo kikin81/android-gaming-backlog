b0f44761c1b1c91a6ed4af81154092a73d7bf060
Francisco <fvelazquez@turo.com>
2024-05-17 18:27:14 +0000

    chore: delete unused code

--- app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
+++ app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
@@ -55,19 +55,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun GreetingPreview() {
-    AppTheme {
-        Greeting("Android")
-    }
-}
