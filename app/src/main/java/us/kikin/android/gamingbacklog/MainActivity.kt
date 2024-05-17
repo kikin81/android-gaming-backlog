5820b18c299c35ca5720def45badd69802c6cef3
Francisco Velazquez <kikin81@gmail.com>
2024-05-17 08:45:54 -0700

    feat(onboarding): basic onboarding and edge to edge (#4)

--- app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
+++ app/src/main/java/us/kikin/android/gamingbacklog/MainActivity.kt
@@ -4,28 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import us.kikin.android.gamingbacklog.presentation.onboarding.OnboardingScreen
 import us.kikin.android.gamingbacklog.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnboardingScreen()
                 }
             }
         }
